b7b7194df859e8e322e6cc915277f4de4294a1b1
birandkoray <birandkoray@gmail.com>
2018-06-29 20:58:51 +0300

    style changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {COUNTRIES} from './data/countries';
 
 class App extends Component {
 
-  state = { results : [] }
+  state = {
+    results: []
+  }
 
   componentDidMount() {
-    fetch('/wc18')
-      .then(res => res.json())
-      .then(x => { this.setState({results : x.results})});
+    fetch('/wc18').then(res => res.json()).then(x => {
+      this.setState({results: x.results})
+    });
 
+  }
+  capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
   }
 
   render() {
-    const groups = this.state.results.map( (res , index) =>
-    {
+    const groups = this.state.results.map((res, index) => {
 
       const groupResults = res.results;
 
+      const elGroupResults = groupResults.map((result, elIndex) => {
+        let [home, away] = Object.keys(result);
 
-      const elGroupResults = groupResults.map ((result , elIndex) => {
-         let [home , away] = Object.keys(result);
+        return (<div className="result">
+          <div className="homet">
+            <span>{this.capitalize(COUNTRIES[home])}</span>
+          </div>
+          <div className="home">
+            <img className="home-icon" alt="" src={`icons/${home}.png`}/>
+          </div>
+          <div className="score">
+            <span>{result[home]}
+              - {result[away]}</span>
+          </div>
+          <div className="away">
+            <img className="away-icon" alt="" src={`icons/${away}.png`}/>
+          </div>
+          <div className="awayt">
+            <span>{this.capitalize(COUNTRIES[away])}</span>
+          </div>
 
-
-         return (<div className="result">
-           <div className="homet">
-             <span>{COUNTRIES[home].toUpperCase()}</span>
-           </div>
-           <div className="home">
-               <img className="home-icon" alt="" src={`icons/${home}.png`}/>
-           </div>
-           <div className="score">
-              <span>{result[home]} - {result[away]}</span>
-           </div>
-           <div className="away">
-               <img className="away-icon" alt="" src={`icons/${away}.png`}/>
-           </div>
-           <div className="awayt">
-             <span>{COUNTRIES[away].toUpperCase()}</span>
-           </div>
-
-         </div>)
+        </div>)
 
       })
 
       return (<div key={index}>
         {elGroupResults}
       </div>)
-    }
-
-    );
-    return (
-      <div>
-        {groups}
-      </div>
-    );
+    });
+    return (<div>
+      {groups}
+    </div>);
   }
 }
 
