91cae1c1a8a4af977abc44ac7837276ae43117f9
birandkoray <birandkoray@gmail.com>
2018-07-07 00:14:17 +0300

    knockout stage page vue init

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const fetch = require('node-fetch');
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
+const serveStatic = require('serve-static');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname , 'client/build')));
+app.use(express.static(path.join(__dirname , 'knockout/dist')));
 
 app.get('/wc18' , (req,res) => {
   fetch('https://s3-us-west-2.amazonaws.com/fifaworldcup2018rest/wc18.json')
@@ -15,9 +17,13 @@ app.get('/wc18' , (req,res) => {
    .catch((ex) => console.log('parsing failed' , ex))
 });
 
-app.get('/groupStage' , (req,res) => {
+app.get('/group' , (req,res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
+
+app.get('/knockout' , (req , res) => {
+  res.sendFile(path.join(__dirname + '/knockout/dist/index.html'));
+})
 
 app.get('*' , (req , res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
