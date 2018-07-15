12adbec885fca31906187dc093e7e4f80c5c15f6
birandkoray <birandkoray@gmail.com>
2018-07-15 22:21:27 +0300

    wc2018 final result updated

--- knockout/src/components/Final.vue
+++ knockout/src/components/Final.vue
@@ -12,7 +12,7 @@
       <li class="spacer">&nbsp;</li>
       <li class="spacer">&nbsp;</li>
       <li data-v-f947c3a8="" class="game game-top winner"><img src="https://aqueous-spire-90124.herokuapp.com/icons/FRA.png" alt="FRA"> FRANCE
-        <span data-v-f947c3a8="">1</span> </li>
+        <span data-v-f947c3a8="">4</span> </li>
     </ul>
     <ul class="round">
       <li class="spacer">&nbsp;</li>
@@ -25,7 +25,7 @@
       <li class="spacer">&nbsp;</li>
       <li class="spacer">&nbsp;</li>
       <li class="spacer title" v-bind:style="{marginLeft:'-22px',paddingBottom:'20px',textAlign:'left'}">FINAL</li>
-      <li class="game game-top"><span v-bind:style="{float:'left',marginRight:'20px'}">0</span> CROATIA <img src="https://aqueous-spire-90124.herokuapp.com/icons/CRO.png" alt="CRO">
+      <li class="game game-top"><span v-bind:style="{float:'left',marginRight:'20px'}">2</span> CROATIA <img src="https://aqueous-spire-90124.herokuapp.com/icons/CRO.png" alt="CRO">
          </li>
     </ul>
     </ul>
